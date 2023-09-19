b40ff7f618c780f5f36d7d8e0f829e1807019626
joao amorim <jpamorim811@gmail.com>
2023-09-19 11:49:23 -0400

    testeJs

---
+++ script.js
@@ -0,0 +1,32 @@
+var sorteioDisplay = document.getElementById("sorteio");
+var sortearBotao = document.getElementById("sortearBotao");
+var arrayNomes = [ "House","Jujutsu", "TheBoys", "Aot"];
+
+
+
+// script.js
+
+const slides = document.querySelectorAll('.carousel-slide');
+let currentSlide = 0;
+function showSlide(n) {
+    slides.forEach(slide => {
+        slide.style.display = 'none';
+    });
+    currentSlide = (n + slides.length) % slides.length;
+    slides[currentSlide].style.display = 'block';
+    
+}
+
+sortearBotao.addEventListener("click", function() {
+    var numeroAleatorio = Math.floor(Math.random() * 4) + 0; // Gera um número aleatório entre 1 e 100
+    if(arrayNomes[numeroAleatorio] = 0){
+        elementoHTML.style.fontFamily = fontes[elemento];
+    }
+    
+    sorteioDisplay.textContent =  arrayNomes[numeroAleatorio];
+    showSlide(currentSlide=numeroAleatorio);
+    
+});
+
+
+
